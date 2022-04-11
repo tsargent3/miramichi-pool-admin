89fadbe92ad4cdc1b120f49e705159d00805624e
Tyler Sargent <tyler.sargent@outlook.com>
2022-04-11 20:36:16 -0300

    Add home link to logo and title

--- front-end/src/components/SidebarPlugin/SideBar.vue
+++ front-end/src/components/SidebarPlugin/SideBar.vue
@@ -6,13 +6,14 @@
     :style="sidebarStyle"
   >
     <div class="logo">
-      <a href="#" class="simple-text logo-mini">
+      <a href="#/dashboard/" class="simple-text logo-mini">
         <div class="logo-img">
           <img :src="imgLogo" alt="" />
         </div>
       </a>
 
       <span
+        href="#/dashboard/"
         class="simple-text logo-normal"
       >
         {{ title }}
